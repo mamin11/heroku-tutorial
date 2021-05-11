9c439eef8e357961da8f5dca96b75947982409eb
mamin11 <abdimamin@gmail.com>
2021-05-11 16:49:09 +0100

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function home() {
+        $users = User::paginate(10);
+        return view('welcome')->with([
+            'users' => $users
+        ]);
+    }
+}
